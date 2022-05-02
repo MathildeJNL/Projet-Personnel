40a34cab0284052f9617cebb195158ff729dde89
Mathilde Jnl <mathilde.jeannolle@gmail.com>
2022-05-02 15:25:19 +0200

    JS file creation for alert

---
+++ script.js
@@ -0,0 +1,19 @@
+let projectOne = document.getElementById("projectOne");
+projectOne.addEventListener("click", function(event){
+    event.preventDefault();
+        alert("Ce projet n\'est pas disponible pour éviter tout plagiat. (Recommandation OpenClassrooms)");
+})
+
+const errorMess = "Ce projet n\'est pas encore disponible... Learning in progress :)";
+
+let projectFive = document.getElementById("projectFive");
+projectFive.addEventListener("click", function(event){
+    event.preventDefault();
+        alert(errorMess);
+})
+
+let projectSix = document.getElementById("projectSix");
+projectSix.addEventListener("click", function(event){
+    event.preventDefault();
+        alert(errorMess);
+})
